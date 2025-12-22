197b8c9f02ff96ee30dee0fc44ce7edd9b95d4ee
veeolet-ml <veeolet.stan@gmail.com>
2025-12-22 15:21:21 +0200

    Changed os.environ.get to os.getenv

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ load_dotenv(BASE_DIR / '.env')
 db_url = os.getenv('DATABASE_URL', 'sqlite:///site.db')
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'gigel')
-    DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'gigel')
+    DEBUG = os.getenv('DEBUG', 'False') == 'True'
     SQLALCHEMY_DATABASE_URI = db_url.replace("sqlite:///", f"sqlite:///{BASE_DIR}/")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
